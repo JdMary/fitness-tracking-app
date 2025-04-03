7a18c0eb628cc791569f98971833fabed4ea8ac8
farahAddad <farah.addad@esprit.tn>
2025-04-03 13:12:39 +0100

    changed security filter

--- backend/user/src/main/java/fitrack/user/security/SecurityFilter.java
+++ backend/user/src/main/java/fitrack/user/security/SecurityFilter.java
@@ -6,7 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -40,7 +40,7 @@ public class SecurityFilter extends OncePerRequestFilter {
                 throw new UsernameNotFoundException("User not found: " + subject);
             }
         }
-
+        filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request) {
