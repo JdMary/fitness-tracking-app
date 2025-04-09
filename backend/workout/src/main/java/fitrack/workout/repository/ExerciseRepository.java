a4a68ef087f16349293ab81f9c9ddb58caf664e1
farahAddad <farah.addad@esprit.tn>
2025-04-09 19:20:55 +0100

    integrer fronted(crud)

--- backend/workout/src/main/java/fitrack/workout/repository/ExerciseRepository.java
+++
@@ -1,7 +0,0 @@
-package fitrack.workout.repository;
-
-import fitrack.workout.entity.Exercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExerciseRepository extends JpaRepository<Exercise,String> {
-}
